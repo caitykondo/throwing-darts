9352fae8f21b52d8b5244d30293a16f1793cf5cb
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-02-18 09:53:50 -1000

    added scoring case where all throws are less than 5 and when an empty array is given

--- scoreThrows.js
+++ scoreThrows.js
@@ -4,21 +4,27 @@
 
 
 function scoreThrows(array) {
-  points = 0;
+  let points = 0;
+  let lessThan5 = true;
   array.forEach((radius, index, array)=>{
     if(radius > 10 ){
       points += 0;
+      lessThan5 = false;
     }else if(radius >= 5 && radius <= 10){
-       points += 5;
+      points += 5;
+      lessThan5 = false;
     }else if (radius < 5){
       points += 10;
     }
   });
+  if (lessThan5 === true && array.length !== 0){
+    points += 100;
+  }
   return points;
 }
 
-let r1 = [1, 5, 11]; //15
+let r1 = [1, 5, 11]; //15ss
 let r2 = [15, 20, 30]; //0
 let r3 = [1, 2, 3, 4]; //140
 
-console.log(scoreThrows(r1));
+console.log(scoreThrows([]));
